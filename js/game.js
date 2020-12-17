8dd461675abd8ec8a469a7d3ae8dafdaf2909dd1
Md Tausif Iqbal <mdtausifiqbalnwd@gmail.com>
2020-12-17 17:23:28 +0530

    added levels

--- js/game.js
+++ js/game.js
@@ -4,8 +4,9 @@ var game = {
         game.canvas = document.getElementById("gamecanvas");
         game.context = game.canvas.getContext("2d");
 
+        // Initialize Objects
+        levels.init();
         // Hide all game layers and display the start Screen
-
         game.hideScreens();
         game.showScreen("gamestartscreen");
     },
@@ -29,5 +30,52 @@ var game = {
     showScreen: function (id) {
         var screen = document.getElementById(id);
         screen.style.display = "block";
+    },
+
+    showLevelScreen: function name() {
+        game.hideScreens();
+        game.showScreen("levelselectscreen");
+    }
+}
+
+var levels = {
+    data: [
+        {
+            // Level 1
+            foreground:"desert-foreground",
+            background:"clouds-background",
+            entities: []
+        },
+        {
+            // Level 2
+            foreground:"desert-foreground",
+            background:"clouds-background",
+            entities: []
+        },
+    ],
+    init: function () {
+        var levelSelectScreen = document.getElementById("levelselectscreen");
+
+        // An event handler to call
+        var buttonClickHandler = function () {
+            game.hideScreen("levelselectscreen");
+
+            // Load Level
+            levels.load(this.value - 1);
+        }
+
+        for (let i = 0; i < levels.data.length; i++) {
+            var button = document.createElement("input");
+            button.type = "button";
+            button.value = (i + 1);
+            button.addEventListener("click", buttonClickHandler);
+
+            levelSelectScreen.appendChild(button);
+            
+        }
+    },
+
+    load: function (number) {
+        
     }
 }
